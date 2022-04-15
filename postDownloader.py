ff6570c0422b6bf9cc22d89c9e987e20f33093b4
farhibahsan <farhib.ahsan@gmail.com>
2022-04-15 14:30:18 -0400

    Update postDownloader.py

--- postDownloader.py
+++ postDownloader.py
@@ -1,4 +1,5 @@
-# Grabbed from: https://github.com/Watchful1/Sketchpad/blob/master/postDownloader.py
+# Original script taken from Watchful1 at the following link: https://github.com/Watchful1/Sketchpad/blob/master/postDownloader.py
+# Modified to grab certain date ranges of comments/posts
 
 import requests
 from datetime import datetime
@@ -24,21 +25,23 @@ elif username != "" and subreddit == "":
 else:
 	filter_string = f"author={username}&subreddit={subreddit}"
 
-url = "https://api.pushshift.io/reddit/{}/search?limit=1000&sort=desc&{}&after=1580619600&before="
+url = "https://api.pushshift.io/reddit/{}/search?limit=1000&sort=desc&{}&after={}&before="
 
 start_time = datetime.utcnow()
 
 
-def downloadFromUrl(filename, object_type, subreddit):
+def downloadFromUrl(filename, object_type, sub, start_time, end_time):
 	print(f"Saving {object_type}s to {filename}")
 
 	count = 0
 	handle = open(filename, 'w')
-	previous_epoch = 1597204800
+	previous_epoch = end_time
+
+	subreddit = f"subreddit={sub}"
 
 	handle.write("[")
 	while True:
-		new_url = url.format(object_type, subreddit)+str(previous_epoch)
+		new_url = url.format(object_type, subreddit, start_time)+str(previous_epoch)
 		json_text = requests.get(new_url, headers={'User-Agent': "Post downloader by /u/Watchful1"})
 		time.sleep(1)  # pushshift has a rate limit, if we send requests too fast it will start returning error messages
 		try:
@@ -73,7 +76,8 @@ def downloadFromUrl(filename, object_type, subreddit):
 					new_json['body'] = object['body']
 					new_json['locked'] = object['locked']
 					json.dump(new_json, handle)
-					handle.write(",")
+					if previous_epoch != start_time + 1:
+						handle.write(",")
 				except Exception as err:
 					print(f"Couldn't print comment: https://www.reddit.com{object['permalink']}")
 					print(traceback.format_exc())
@@ -94,14 +98,15 @@ def downloadFromUrl(filename, object_type, subreddit):
 
 		print("Saved {} {}s through {}".format(count, object_type, datetime.fromtimestamp(previous_epoch).strftime("%Y-%m-%d")))
 
-	handle.write('{\"retrieved_on\": 0, \"link_id\": \"\", \"parent_id\": \"\", \"author\": \"\", \"body\": \"\", \"locked\": false}]')
+	# handle.write('{\"retrieved_on\": 0, \"link_id\": \"\", \"parent_id\": \"\", \"author\": \"\", \"body\": \"\", \"locked\": false}]')
+	handle.write("]")
 	print(f"Saved {count} {object_type}s")
 	handle.close()
 
 
 # downloadFromUrl("posts.txt", "submission")
-downloadFromUrl("sandersforpresident.json", "comment", filter_string)
-filter_string = "subreddit=joebiden"
-downloadFromUrl("joebiden.json", "comment", filter_string)
-statistics("sandersforpresident.json", "sanders_stats.txt")
-statistics("joebiden.json", "biden_stats.txt")
+# downloadFromUrl("sandersforpresident.json", "comment", filter_string)
+# filter_string = "subreddit=joebiden"
+# downloadFromUrl("joebiden.json", "comment", filter_string)
+# statistics("sandersforpresident.json", "sanders_stats.txt")
+# statistics("joebiden.json", "biden_stats.txt")
